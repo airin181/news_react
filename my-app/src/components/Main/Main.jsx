cb86d8db0cffd02418cc18b6d37ee39117cee596
airin181 <98608110+airin181@users.noreply.github.com>
2022-04-27 15:59:10 +0200

    ADD: routing

--- my-app/src/components/Main/Main.jsx
+++ my-app/src/components/Main/Main.jsx
@@ -2,13 +2,16 @@ import React, { Component } from "react";
 import Form from './components/Main/Form'
 import Home from './components/Main/Home'
 import News from './components/Main/News'
+import {Route, Routes} from 'react-router-dom'
 
 class Main extends Component {
   render() {
     return <div>
-      <Home/>
-      <Form/>
-      <News/>
+      <Routes>
+      <Route element={<Home/>} path='/'/>
+      <Route element={<Form/>} path='/form'/>
+      <Route element={<News/>} path='/news-list'/>
+      </Routes>
 
     </div>;
   }

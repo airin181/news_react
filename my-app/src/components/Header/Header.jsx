f17eac4edb06c396939ce5a703364cb8093e47af
airin181 <98608110+airin181@users.noreply.github.com>
2022-04-27 14:47:21 +0200

    ADD: folder structure

---
+++ my-app/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Nav from './components/Nav'
+
+class Header extends Component {
+  render() {
+    return <div>
+      <Nav/>
+    </div>;
+  }
+}
+
+export default Header;

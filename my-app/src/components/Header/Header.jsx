f98676d039a76bcf2f9e9f19fe2d0a62d2f2878c
airin181 <98608110+airin181@users.noreply.github.com>
2022-04-27 16:21:54 +0200

    ADD: logo

--- my-app/src/components/Header/Header.jsx
+++ my-app/src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Nav from './Nav';
+import logo from '../../assets/logo.svg';
+import './Header.css'
 
 class Header extends Component {
   render() {
-    return <div>
-      <img src="" alt="" />
+    return <div className="header">
+      <img src={logo} alt="logo newspaper" className="logo"/>
       <Nav/>
     </div>;
   }
